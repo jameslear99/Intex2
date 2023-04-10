98bce4c44465d8c24895c1b2795d98011c1858bf
izzysampsonn <isabellesampson3@gmail.com>
2023-04-10 09:46:58 -0600

    Password authentication stuff

--- Intex2/Startup.cs
+++ Intex2/Startup.cs
@@ -34,7 +34,18 @@ namespace Intex2
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
-        }
+
+            // Password authentication stuff
+            services.Configure<IdentityOptions>(options =>
+                {
+                    options.Password.RequireDigit = true;
+                    options.Password.RequireLowercase = true;
+                    options.Password.RequireNonAlphanumeric = true;
+                    options.Password.RequireUppercase = true;
+                    options.Password.RequiredLength = 8;
+                    options.Passowrd.RequiredUniqueChars = 1;
+                });
+            }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
