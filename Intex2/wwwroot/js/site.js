cc9bb6942cd0ca5817b8ba84968392c7c7ebc78e
chase.rindlisbacher <cjrjazz20@gmail.com>
2023-04-12 20:00:16 -0600

    trying to implement confirmation buttons

--- Intex2/wwwroot/js/site.js
+++ Intex2/wwwroot/js/site.js
@@ -2,3 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function confirmDelete() {
+    var confirmed = confirm("Are you sure you want to delete this entry?");
+    if (confirmed) {
+        // User confirmed, submit the form
+        document.getElementById("delete-form").submit();
+    } else {
+        // User canceled, do nothing
+        return false;
+    }
+}
+
+function confirmEdit() {
+    var confirmed = confirm("Are you sure you'd like to submit these changes?");
+    if (confirmed) {
+        // User confirmed, submit the form
+        document.getElementById("edit-form").submit();
+    } else {
+        // User canceled, do nothing
+        return false;
+    }
+}
+
+function commenceEdit() {
+    var confirmed = confirm("Are you sure you'd like to edit this entry?");
+    if (confirmed) {
+        // User confirmed, submit the form
+        document.getElementById("edit-form").submit();
+    } else {
+        // User canceled, do nothing
+        return false;
+    }
+}
